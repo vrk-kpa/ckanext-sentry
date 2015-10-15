02868d62b76cdf21ae363154cc2c29207a1946d7
amercader <amercadero@gmail.com>
2015-10-15 14:43:33 +0100

    Use new error log middleware hooks

--- ckanext/sentry/plugins.py
+++ ckanext/sentry/plugins.py
@@ -11,8 +11,16 @@ log = logging.getLogger(__name__)
 
 class SentryPlugin(plugins.SingletonPlugin):
     '''A simple plugin that add the Sentry middleware to CKAN'''
-    plugins.implements(plugins.IMiddleware)
+    plugins.implements(plugins.IMiddleware, inherit=True)
 
     def make_middleware(self, app, config):
+        if plugins.toolkit.check_ckan_version('2.3'):
+            return app
+        else:
+            return self.make_error_log_middleware(app, config)
+
+    def make_error_log_middleware(self, app, config):
         from raven.contrib.pylons import Sentry
+
+        log.debug('Adding Sentry middleware...')
         return Sentry(app, config)
