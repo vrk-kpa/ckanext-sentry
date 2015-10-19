4bc444a314f0551a4383e11c2cd92699018ecd36
amercader <amercadero@gmail.com>
2015-10-19 12:54:44 +0100

    Allow to automatically configure the Sentry logging
    As an alternative to manually setting it on the ini file as described in
    
    http://raven.readthedocs.org/en/latest/integrations/pylons.html
    
    Based on @rshk work on
    https://github.com/opendatatrentino/ckanext-sentry

--- ckanext/sentry/plugins.py
+++ ckanext/sentry/plugins.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from raven.contrib.pylons import Sentry
+from raven.handlers.logging import SentryHandler
+
 import logging
 
 from ckan import plugins
@@ -20,7 +23,31 @@ class SentryPlugin(plugins.SingletonPlugin):
             return self.make_error_log_middleware(app, config)
 
     def make_error_log_middleware(self, app, config):
-        from raven.contrib.pylons import Sentry
+
+        if plugins.toolkit.asbool(config.get('sentry.configure_logging')):
+            self._configure_logging(config)
 
         log.debug('Adding Sentry middleware...')
         return Sentry(app, config)
+
+    def _configure_logging(self, config):
+        '''
+        Configure the Sentry log handler to the specified level
+
+        Based on @rshk work on
+        https://github.com/opendatatrentino/ckanext-sentry
+        '''
+
+        handler = SentryHandler(config.get('sentry.dsn'))
+        handler.setLevel(logging.NOTSET)
+
+        loggers = ['', 'ckan', 'ckanext', 'sentry.errors']
+        sentry_log_level = config.get('sentry.log_level', logging.INFO)
+        for name in loggers:
+            logger = logging.getLogger(name)
+            logger.addHandler(handler)
+            if name == 'sentry.errors':
+                logger.setLevel(sentry_log_level)
+
+        log.debug('Setting up Sentry logger with level {0}'.format(
+            sentry_log_level))
