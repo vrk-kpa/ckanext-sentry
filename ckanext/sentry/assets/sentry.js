3557c651b82274e38c03537ff64522527288e697
Jari Voutilainen <jari.voutilainen@iki.fi>
2022-05-23 09:16:57 +0300

    Remove logging

--- ckanext/sentry/assets/sentry.js
+++ ckanext/sentry/assets/sentry.js
@@ -1,6 +1,5 @@
 let dsn = document.querySelector("[data-sentry]").getAttribute('data-dsn')
 let environment = document.querySelector("[data-sentry]").getAttribute('data-environment')
-console.log(dsn)
 Sentry.init({
     dsn: dsn,
     release: "",
